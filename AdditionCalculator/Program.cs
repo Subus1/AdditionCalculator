c5877527d4772319f93e07a83fbaf946933f6178
Subus1 <20029920@students.koi.edu.au>
2025-08-10 17:57:14 +1000

    Add addition calculator

---
+++ AdditionCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Addition Calculator ===");
+
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num1 + num2;
+
+        Console.WriteLine($"Result: {num1} + {num2} = {result}");
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
